5c81aa62b5888a0745ce9bc9b8e8233c1f5f1221
juhkast <juhkast@laurea.fi>
2026-08-18 10:15:23 +0300

    new files

--- newfile.js
+++ newfile.js
@@ -1,3 +1,10 @@
 const name = "Juho";
 
 console.log(`Hello, ${name}!`);
+
+const button = document.querySelector("#button");
+const message = document.querySelector("#message");
+
+button.addEventListener("click", () => {
+    message.textContent = `Hello, ${name}! You clicked the button.`;
+});
